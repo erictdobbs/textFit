2769e9ccd16e99c26e9a7ca86102a3a18ab7a698
Eric Dobbs <erictdobbs@gmail.com>
2013-05-22 16:51:57 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+using System.Drawing; 
+
+namespace ImageManipulation
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+
+
+
+
+    }
+}
